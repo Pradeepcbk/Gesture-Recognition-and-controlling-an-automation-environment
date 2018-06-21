180b8dbd216b59cff0836eeaadb960c3c362dd52
Pradeep <34934343+Pradeepcbk@users.noreply.github.com>
2018-06-21 18:46:00 +0200

    Delete removeLastRows.py

--- Code/src/Task2/removeLastRows.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-import csv
-with open('first.csv', 'rb') as inp, open('first_edit.csv', 'wb') as out:
-    writer = csv.writer(out)
-    for row in csv.reader(inp):
-	writer.writerow(row)
